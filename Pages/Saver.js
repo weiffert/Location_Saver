81b4501c118e61a18be931a2fc0eb6452bd17ccd
William Eiffert <weiffert@gmail.com>
2019-01-18 16:22:35 -0500

    Add Basic Pages

---
+++ Pages/Saver.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class Saver extends React.Component {
+  render() {
+    return <Text>Saver</Text>;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ Pages/Viewer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class Viewer extends React.Component {
+  render() {
+    return <Text>View</Text>;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
