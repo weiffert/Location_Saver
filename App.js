a11c5f340772e7703ab81c73ffca658da795e7df
William Eiffert <weiffert@gmail.com>
2019-01-18 16:26:28 -0500

    Import Pages

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import Home from "./Pages/Home"
+import Viewer from "./Pages/Viewer"
+import Saver from "./Pages/Saver"
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: "Home",
+    };
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Home />
+        <Viewer />
+        <Saver />
+        {/*
+        <View>
+          <Button
+            onPress={this.setState({ page: "Viewer" })}
+            title="View Locations"
+            disabled={true}
+          />
+          <Button
+            onPress={this.setState({ page: "Saver" })}
+            title="Save Location"
+          />
+        </View>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+        */}
       </View>
     );
   }
@@ -14,8 +42,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
